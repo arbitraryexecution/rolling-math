0de6e772e6139d68f04626696571ca9e1a8991a5
yowl <yowl@arbitraryexecution.com>
2021-08-26 02:42:53 -0700

    created NPM package and copied code over

---
+++ src/rolling.math.js
@@ -0,0 +1,116 @@
+const BigNumber = require("bignumber.js");
+const assert = require("assert");
+
+// in memory rolling average calculator
+module.exports = class RollingMath {
+  constructor(windowSize) {
+    if (!windowSize) {
+      throw "You must provide a valid window size";
+    }
+    this.windowSize = windowSize;
+    this.elements = new Array(windowSize);
+    this.nextIdx = 0;
+    this.sum = new BigNumber(NaN);
+    this.mean = new BigNumber(NaN);
+    this.variance = new BigNumber(NaN);
+  }
+
+  // returns number of elements
+  getNumElements() {
+    return this.elements[this.windowSize - 1] == undefined ? this.nextIdx : this.windowSize;
+  }
+
+  // returns windowSize;
+  getWindowSize() {
+    return this.windowSize;
+  }
+
+  // returns the sum of the past windowSize elements
+  getSum() {
+    return this.sum;
+  }
+
+  // returns rolling average of the past windowSize elements
+  getAverage() {
+    return this.mean;
+  }
+
+  // returns rolling standard deviation of  the past windowSize elements
+  getStandardDeviation() {
+    return this.variance.sqrt();
+  }
+
+  // adds an element into the rolling average
+  addElement(element) {
+    assert(BigNumber.isBigNumber(element), "Elements must be BigNumbers");
+    assert(element.isFinite(), "Elements cannot be non-finite");
+
+    if (this.elements[this.windowSize - 1] == undefined) {
+      this.#addElementNonFullList(element);
+    } else {
+      this.#addElementFullList(element);
+    }
+  }
+
+  // adds an element to the rolling average when average is full
+  #addElementFullList(element) {
+    // update the sum
+    this.sum = this.sum.minus(this.#currElement());
+    this.sum = this.sum.plus(element);
+
+    // update the mean
+    const oldMean = this.mean;
+    const diff = element.minus(this.#currElement());
+    this.mean = oldMean.plus(diff.div(this.windowSize));
+
+    // update the std
+    const newDeviation = element.minus(this.mean);
+    const oldDeviation = this.#currElement().minus(oldMean);
+
+    this.variance = this.variance.plus((diff.times(newDeviation.plus(oldDeviation)).div(this.windowSize - 1)));
+
+    // Rounding can cause the variance to dip slightly below zero, make sure this never happens
+    if (this.variance.isLessThan(0)) {
+      this.variance = new BigNumber(0);
+    }
+
+    // replace the oldest element with this one
+    this.elements[this.nextIdx] = element;
+    this.#incIdx();
+  }
+
+  // adds an element to the rolling average when average isn't full
+  #addElementNonFullList(element) {
+    // if it is the first element, initialize our variables
+    if(this.elements[0] == undefined) {
+      this.sum = element;
+      this.mean = element;
+      this.variance = new BigNumber(0);
+    } else {
+      // Welford's algorithm
+      const numElements = this.nextIdx;
+      const oldDeviation = element.minus(this.mean);
+
+      this.sum = this.sum.plus(element);
+      this.mean = this.mean.plus(oldDeviation.div(numElements + 1));
+
+      const newDeviation = element.minus(this.mean);
+      const deviationOfDeviation = (oldDeviation.times(newDeviation)).minus(this.variance);
+      this.variance = this.variance.plus(deviationOfDeviation.div(numElements));
+    }
+
+    // insert element to list
+    this.elements[this.nextIdx] = element;
+    this.#incIdx();
+  }
+
+  // returns current element
+  #currElement() {
+    return this.elements[this.nextIdx];
+  }
+
+  // increments nextIdx
+  #incIdx() {
+    this.nextIdx = (this.nextIdx + 1) % this.windowSize;
+  }
+};
---
+++ src/rolling.math.spec.js
@@ -0,0 +1,161 @@
+const BigNumber = require("bignumber.js");
+const RollingMath = require("./rolling.math");
+
+const bigZero = new BigNumber(0);
+const bigNaN = new BigNumber(NaN);
+
+const testElements = [ new BigNumber(100),
+                       new BigNumber(110),
+                       new BigNumber(40),
+                       new BigNumber(55),
+                       new BigNumber(45),
+                       new BigNumber(70) ];
+
+const windowSize = 5;
+
+let a = new BigNumber("10");
+
+describe("RollingMath library", () => {
+  let rollingMath;
+
+  describe("init", () => {
+    it("should throw on creation if not provided a windowSize", () => {
+      expect(() => {
+        new RollingMath();
+      }).toThrow();
+    });
+
+    it("new object is correctly created when passed windowSize", () => {
+      rollingMath = new RollingMath(windowSize);
+
+      expect(rollingMath.elements).toEqual(new Array(windowSize));
+      expect(rollingMath.nextIdx).toEqual(0);
+
+      expect(rollingMath.getWindowSize()).toEqual(windowSize);
+      expect(rollingMath.getSum()).toEqual(bigNaN);
+      expect(rollingMath.getAverage()).toEqual(bigNaN);
+      expect(rollingMath.getStandardDeviation()).toEqual(bigNaN);
+    });
+  });
+
+  describe("operation", () => {
+    beforeEach(() => {
+      rollingMath = new RollingMath(windowSize);
+
+      // adding without hitting windowSize
+      for(let i = 0; i < windowSize; i++) {
+        rollingMath.addElement(testElements[i]);
+      }
+
+    });
+
+    describe("addElement", () => {
+      it("test addElement", () => {
+        // test adding invalid elements
+        expect(() => {
+          rollingMath.addElement(10);
+        }).toThrow();
+
+        expect(() => {
+          rollingMath.addElement(new BigNumber(NaN));
+        }).toThrow();
+
+        expect(() => {
+          rollingMath.addElement(new BigNumber(Infinity));
+        }).toThrow();
+
+        expect(() => {
+          rollingMath.addElement(new BigNumber(-Infinity));
+        }).toThrow();
+
+        // check if we successfully added elements in the beforeEach
+        expect(rollingMath.elements[0]).toEqual(testElements[0]);
+
+        // adding after hitting windowSize
+        rollingMath.addElement(testElements[windowSize]);
+
+        expect(rollingMath.elements[0]).toEqual(testElements[windowSize]);
+      });
+    });
+
+    describe("getSum", () => {
+      it("test getSum", () => {
+        expect(rollingMath.getSum()).toEqual(BigNumber.sum(...rollingMath.elements));
+
+        let preWindowSize = rollingMath.getSum();
+
+        // adding after hitting windowSize
+        rollingMath.addElement(testElements[windowSize]);
+
+        expect(rollingMath.getSum()).toEqual(BigNumber.sum(...rollingMath.elements));
+
+        // ensure they are different values
+        expect(rollingMath.getSum()).not.toEqual(preWindowSize);
+      });
+    });
+
+    describe("getAverage", () => {
+      it("test getAverage", () => {
+        expect(rollingMath.getAverage()).toEqual(BigNumber.sum(...rollingMath.elements).div(windowSize));
+
+        let preWindowSize = rollingMath.getAverage();
+
+        // adding after hitting windowSize
+        rollingMath.addElement(testElements[windowSize]);
+
+        expect(rollingMath.getAverage()).toEqual(BigNumber.sum(...rollingMath.elements).div(windowSize));
+
+        // ensure they are different values
+        expect(rollingMath.getAverage()).not.toEqual(preWindowSize);
+      });
+    });
+
+    describe("getStandardDeviation", () => {
+      it("test getStandardDevation", () => {
+        // calculate the true variance
+        let squares = rollingMath.elements.map(x => x.minus(rollingMath.getAverage()).pow(2));
+        let sumOfSquares = BigNumber.sum(...squares);
+        let variance = sumOfSquares.div(windowSize - 1);
+
+        // we have to round these to account for negligable rounding errors when using square root and
+        // division
+        expect(rollingMath.variance.toFixed(8)).toEqual(variance.toFixed(8));
+        expect(rollingMath.getStandardDeviation().toFixed(8)).toEqual(variance.sqrt().toFixed(8));
+
+        let preWindowSize = rollingMath.getStandardDeviation();
+
+        // adding after hitting windowSize
+        rollingMath.addElement(testElements[windowSize]);
+
+        // calculate the true variance
+        squares = rollingMath.elements.map(x => x.minus(rollingMath.getAverage()).pow(2));
+        sumOfSquares = BigNumber.sum(...squares);
+        variance = sumOfSquares.div(windowSize - 1);
+
+        // we have to round these to account for negligable rounding errors when using square root and
+        // division
+        expect(rollingMath.variance.toFixed(8)).toEqual(variance.toFixed(8));
+        expect(rollingMath.getStandardDeviation().toFixed(8)).toEqual(variance.sqrt().toFixed(8));
+
+        // ensure they are different values
+        expect(rollingMath.getStandardDeviation()).not.toEqual(preWindowSize);
+      });
+
+      it("test negative variance", () => {
+        // Due to miniscule rounding desparancies our function can return a negative variance which is
+        // impossible. Make sure we can never return a negative variance.
+        //
+        // We will see negative variance when we update a non-zero variance to zero by pushing windowSize
+        // number elements of the same value
+
+        // add windowSize number of elements
+        for(let i = 0; i < windowSize; i++) {
+          rollingMath.addElement(bigZero);
+        }
+
+        // variance should be 0 once all elements are the same
+        expect(rollingMath.variance).toEqual(bigZero);
+      });
+    });
+  });
+});
